1e36799fbcbca10c44429ca443b3140f612fd398
pavl <pahenpaveleckaya@gmail.com>
2023-12-21 23:42:59 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import csv, sys, re
+elementsFile = open('periodictable.csv', encoding='utf-8')
+elementsCsvReader = csv.reader(elementsFile)
+elements = list(elementsCsvReader)
+elementsFile.close()
+ALL_COLUMNS = ['Atomic Number', 'Symbol', 'Element', 'Origin of name',
+                'Group', 'Period', 'Atomic weight', 'Density',
+                'Melting point', 'Boiling point',
+                'Specific heat capacity', 'Electronegativity',
+                'Abundance in earth\'s crust']
+LONGEST_COLUMN = 0
+for key in ALL_COLUMNS:
+    if len(key) > LONGEST_COLUMN:
+        LONGEST_COLUMN = len(key)
+ELEMENTS = {}
+for line in elements:
+    element = {'Atomic Number':          line[0],
+               'Symbol':                 line[1],
+               'Element':                line[2],
+               'Origin of name':         line[3],
+               'Group':                  line[4],
+               'Period':                 line[5],
+               'Atomic weight':          line[6] + ' u',  # атомная единица массы
+               'Density':                line[7] + ' g/cm^3', # граммов/куб. см
+               'Melting point':          line[8] + ' K',  # градусов по Кельвину
+               'Boiling point':          line[9] + ' K',  # градусов по Кельвину
+               'Electronegativity':      line[11],
+               'Abundance in earth\'s crust': line[12] + ' mg/kg'}
+for key, value in element.items():
+    element[key] = re.sub(r'\[(I|V|X)+\]', '', value)
+ELEMENTS[line[0]] = element #   атомный номер и элемент
+ELEMENTS[line[1]] = element #    символ и элемент
+print('Periodic Table of Elements')
+print()
+while True: # Основной цикл
+    print('''                                Periodic Table of Elements
+          1  2  3  4  5  6  7  8  9  10  11  12  13  14  15  16  17  18
+        1 H                                                          He
+        2 Li Be                                   B   C   N   O   F  Ne
+        3 Na Mg                                   Al  Si  P   S   Cl Ar
+        4 K  Ca Sc Ti V  Cr Mn Fe Co Ni  Cu  Zn   Ga  Ge  As  Se  Br Kr
+        5 Rb Sr Y  Zr Nb Mo Tc Ru Rh Pd  Ag  Cd   In  Sn  Sb  Te  I  Xe
+        6 Cs Ba La Hf Ta W  Re Os Ir Pt  Au  Hg   Tl  Pb  Bi  Po  At Rn
+        7 Fr Ra Ac Rf Db Sg Bh Hs Mt Ds  Rg  Cn   Nh  Fl  Mc  Lv  Ts Og
+                Ce Pr Nd Pm Sm Eu Gd Tb  Dy  Ho   Er  Tm  Yb  Lu
+                Th Pa U  Np Pu Am Cm Bk  Cf  Es   Fm  Md  No  Lr''')
+    print('Enter a symbol or atomic number to examine, or QUIT to quit.')
+    response = input('> ').title()
+    if response == 'Quit':
+        sys.exit()
+        if response in ELEMENTS:
+            for key in ALL_COLUMNS:
+                keyJustified = key.rjust(LONGEST_COLUMN)
+                print(keyJustified + ': ' + ELEMENTS[response][key])
+                input('Press Enter to continue...')
